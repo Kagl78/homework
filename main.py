c655d0eac29ab6679291914b4a921c43b7250f3b
KarimAgliullin <agliullinkarim1@gmail.com>
2024-10-20 20:09:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 # This is a sample Python script.
+#1st program
+print( 9 ** 0.5 *5)
+#2nd program
+print(9.99 > 9.98 and 1000!= 1000.1)
+#3rd program
+print(2 * 2 + 2 == 2 * (2 + 2))
+#4th program
+print(float(123.456 * 10 ))
+print(float(123.456 * 10))
+print(int(1234.56 % 10))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Привет, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Мир')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print(8 * 10)
